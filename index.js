e4fcac2e871222b562da1e7fb4e7e662e937add2
Roderik van der Veer <roderik@settlemint.com>
2023-05-19 10:13:15 +0200

    debug

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ async function setup() {
 
     // Download the specific version of the tool, e.g. as a tarball/zipball
     const download = getDownloadObject(version);
+    console.log(download)
     const pathToTarball = await tc.downloadTool(download.url);
 
     // Extract the tarball/zipball onto host runner
